a0576a0eb5c51a8e9f5ad25889ebf55ed338bf1d
Alla <alla.slyu@yandex.ru>
2024-02-10 20:06:00 +0300

    Добавлен Boot

--- src/main/java/ru/mts/hw_3/Main.java
+++ src/main/java/ru/mts/hw_3/Main.java
@@ -1,5 +1,7 @@
 package ru.mts.hw_3;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import ru.mts.hw_3.config.AppConfig;
@@ -10,21 +12,24 @@ import ru.mts.hw_3.repository.AnimalsRepositoryImpl;
 import java.util.Arrays;
 import java.util.Set;
 
+@SpringBootApplication
 public class Main {
     public static void main(String[] args) {
-        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
-        AnimalsRepository animalsRepository = context.getBean(AnimalsRepositoryImpl.class);
-
-        System.out.println("findLeapYearNames-------------------------------------------------------------------------------------");
-        System.out.println(Arrays.toString(animalsRepository.findLeapYearNames()) + "\n");
-
-        System.out.println("findOlderAnimal---------------------------------------------------------------------------------------");
-        int age = 15;
-        System.out.println(Arrays.toString(animalsRepository.findOlderAnimal(age)) + "\n");
-
-        System.out.println("findDuplicate-----------------------------------------------------------------------------------------");
-        Set<Animal> animalsDuplicate = animalsRepository.findDuplicate();
-        animalsRepository.printDuplicate();
-        //
+        SpringApplication.run(Main.class, args);
     }
+//    public static void main(String[] args) {
+//        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+//        AnimalsRepository animalsRepository = context.getBean(AnimalsRepositoryImpl.class);
+//
+//        System.out.println("findLeapYearNames-------------------------------------------------------------------------------------");
+//        System.out.println(Arrays.toString(animalsRepository.findLeapYearNames()) + "\n");
+//
+//        System.out.println("findOlderAnimal---------------------------------------------------------------------------------------");
+//        int age = 15;
+//        System.out.println(Arrays.toString(animalsRepository.findOlderAnimal(age)) + "\n");
+//
+//        System.out.println("findDuplicate-----------------------------------------------------------------------------------------");
+//        Set<Animal> animalsDuplicate = animalsRepository.findDuplicate();
+//        animalsRepository.printDuplicate();
+//    }
 }
