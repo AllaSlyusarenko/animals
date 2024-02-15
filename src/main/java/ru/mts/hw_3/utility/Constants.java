392107c01cd61fdf23ee0aa06de962e82cc59581
Alla <alla.slyu@yandex.ru>
2024-02-15 14:28:55 +0300

    удалила лишние файлы

--- src/main/java/ru/mts/hw_3/utility/Constants.java
+++
@@ -1,7 +0,0 @@
-package ru.mts.hw_3.utility;
-
-import java.time.format.DateTimeFormatter;
-
-public class Constants {
-    public static final DateTimeFormatter DATE_FORMATTER_OUT = DateTimeFormatter.ofPattern("dd-MM-yyyy");
-}
