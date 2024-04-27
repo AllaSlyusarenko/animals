d0781cd83bb2acb967fd7297c47178d6ba9e9e7d
Alla <alla.slyu@yandex.ru>
2024-04-27 23:39:53 +0300

    hw-17 - перенесла новое задание в старое, вернула все таблицы из бд

---
+++ src/main/java/ru/mts/hw_3/entity/AnimalType.java
@@ -0,0 +1,91 @@
+package ru.mts.hw_3.entity;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+public class AnimalType {
+    private int idType;
+    private String type;
+    private Boolean isWild;
+    private LocalDate created;
+    private LocalDate updated;
+
+    public AnimalType(int idType, String type, Boolean isWild, LocalDate created, LocalDate updated) {
+        this.idType = idType;
+        this.type = type;
+        this.isWild = isWild;
+        this.created = created;
+        this.updated = updated;
+    }
+
+    public int getIdType() {
+        return idType;
+    }
+
+    public void setIdType(int idType) {
+        this.idType = idType;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Boolean getWild() {
+        return isWild;
+    }
+
+    public void setWild(Boolean wild) {
+        isWild = wild;
+    }
+
+    public LocalDate getCreated() {
+        return created;
+    }
+
+    public void setCreated(LocalDate created) {
+        this.created = created;
+    }
+
+    public LocalDate getUpdated() {
+        return updated;
+    }
+
+    public void setUpdated(LocalDate updated) {
+        this.updated = updated;
+    }
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof AnimalType)) return false;
+
+        AnimalType that = (AnimalType) o;
+        return idType == that.idType && Objects.equals(type, that.type) && Objects.equals(isWild, that.isWild)
+                && Objects.equals(created, that.created) && Objects.equals(updated, that.updated);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = idType;
+        result = 31 * result + Objects.hashCode(type);
+        result = 31 * result + Objects.hashCode(isWild);
+        result = 31 * result + Objects.hashCode(created);
+        result = 31 * result + Objects.hashCode(updated);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "AnimalType{" +
+                "idType=" + idType +
+                ", type='" + type + '\'' +
+                ", isWild=" + isWild +
+                ", created=" + created +
+                ", updated=" + updated +
+                '}';
+    }
+}
---
+++ src/main/java/ru/mts/hw_3/entity/Habitat.java
@@ -0,0 +1,79 @@
+package ru.mts.hw_3.entity;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+public class Habitat {
+    private int idArea;
+    private String area;
+    private LocalDate created;
+    private LocalDate updated;
+
+    public Habitat(int idArea, String area, LocalDate created, LocalDate updated) {
+        this.idArea = idArea;
+        this.area = area;
+        this.created = created;
+        this.updated = updated;
+    }
+
+    public int getIdArea() {
+        return idArea;
+    }
+
+    public void setIdArea(int idArea) {
+        this.idArea = idArea;
+    }
+
+    public String getArea() {
+        return area;
+    }
+
+    public void setArea(String area) {
+        this.area = area;
+    }
+
+    public LocalDate getCreated() {
+        return created;
+    }
+
+    public void setCreated(LocalDate created) {
+        this.created = created;
+    }
+
+    public LocalDate getUpdated() {
+        return updated;
+    }
+
+    public void setUpdated(LocalDate updated) {
+        this.updated = updated;
+    }
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Habitat)) return false;
+
+        Habitat habitat = (Habitat) o;
+        return idArea == habitat.idArea && Objects.equals(area, habitat.area) && Objects.equals(created, habitat.created)
+                && Objects.equals(updated, habitat.updated);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = idArea;
+        result = 31 * result + Objects.hashCode(area);
+        result = 31 * result + Objects.hashCode(created);
+        result = 31 * result + Objects.hashCode(updated);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Habitat{" +
+                "idArea=" + idArea +
+                ", area='" + area + '\'' +
+                ", created=" + created +
+                ", updated=" + updated +
+                '}';
+    }
+}
