7bcfca10f20b37a8085c600c42aebd3100e9017e
Alla <alla.slyu@yandex.ru>
2024-05-08 01:52:49 +0300

    hw-19 - @SpringBootTest(classes = {TestDatabaseConfig.class, ConfigPropertiesForDB.class})   не видит репозиторий, без них контекст не поднимается

--- src/test/java/ru/mts/hw_3/repository/vtoroe/AnimalTest.java
+++ src/test/java/ru/mts/hw_3/repository/vtoroe/AnimalTest.java
@@ -5,29 +5,43 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.testcontainers.junit.jupiter.Testcontainers;
 import ru.mts.hw_3.config.ConfigPropertiesForDB;
 import ru.mts.hw_3.entity.Animal;
 import ru.mts.hw_3.repository.AnimalRepository;
+import ru.mts.hw_3.service.AnimalService;
 import ru.mts.hw_3.service.ServiceFoDB;
 
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+
 @SpringBootTest(classes = {TestDatabaseConfig.class, ConfigPropertiesForDB.class})
-@RunWith(SpringRunner.class)
 @ActiveProfiles("test")
 public class AnimalTest {
 
-    private final AnimalRepository animalRepository;
+    @Autowired
+    private ApplicationContext applicationContext;
+    @Autowired
+    private AnimalRepository animalRepository;
     @Autowired
     private ServiceFoDB serviceFoDB;
 
-    @Autowired
-    public AnimalTest(AnimalRepository animalRepository) {
-        this.animalRepository = animalRepository;
+    public void init() {
+        animalRepository = applicationContext.getBean(AnimalRepository.class);
+    }
+
+    //    @Autowired
+//    public AnimalTest(AnimalRepository animalRepository) {
+//        this.animalRepository = animalRepository;
+//    }
+    @Test
+    public void test() {
     }
 
 
