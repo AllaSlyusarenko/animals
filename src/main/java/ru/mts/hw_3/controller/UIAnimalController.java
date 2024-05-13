fb652493de67699f8a9335d4de104f50412f7f26
Alla <alla.slyu@yandex.ru>
2024-05-14 00:39:27 +0300

    hw-20 создание страницы 2

--- src/main/java/ru/mts/hw_3/controller/UIAnimalController.java
+++ src/main/java/ru/mts/hw_3/controller/UIAnimalController.java
@@ -3,6 +3,7 @@ package ru.mts.hw_3.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import ru.mts.hw_3.service.AnimalService;
 
@@ -14,9 +15,12 @@ public class UIAnimalController {
     public UIAnimalController(AnimalService animalService) {
         this.animalService = animalService;
     }
+
     @GetMapping("/index")
     public String index(Model model) {
-        model.addAttribute("animalList",animalService.getAllAnimals());
+        model.addAttribute("animalList", animalService.getAllAnimals());
         return "index";
     }
+
+//    @DeleteMapping
 }
